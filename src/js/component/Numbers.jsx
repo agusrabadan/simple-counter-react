4fc9437eedc8d1b29a242a06eddf54bf1b288d97
Agustin Rabadán Martínez <agusrabadan85@gmail.com>
2024-04-19 07:46:21 +0000

    practica useState

--- src/js/component/Numbers.jsx
+++ src/js/component/Numbers.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const Numbers = ({number}) => {
+export const Numbers = () => {
+
+    const [number,setNumber] = useState(0);
+    const [color, setColor] = useState("btn btn-warning mx-2 mt-3")
+    const [background, setBackground] = useState("alert alert-primary")
+
+    function sum1(){
+        setNumber(number+1)
+    };
+
+    function dup(){
+        setNumber(number*2)
+    };
+
+    function changeColor(){
+        setColor("btn btn-danger mx-2 mt-3")
+    }
+
+    function reset(){
+        setNumber(0)
+    }
+
+    const fondo = (theme) => {
+        if (theme ==1){
+            setBackground("alert alert-danger")
+            }
+        if (theme ==2){
+            setBackground("alert alert-success")
+        }
+    }
+
+
     return (
-        <div className="nums">{number}</div>
+
+        <div container class={background} role="alert">
+            <h1>{number}</h1>
+            <div className="buttons">
+            <button onClick={sum1} type="button" class="btn btn-primary mx-2 mt-3">+1</button>
+            <button onClick={dup} type="button" class="btn btn-secondary mx-2 mt-3">*2</button>
+            <button onClick={changeColor} type="button" class={color}>Change color</button>
+            <button onClick={reset} type="button" class={color}>Reset</button>
+            <button onClick= {()=> fondo(1)} type="button" class="btn btn-danger mx-2 mt-3">Fondo Rojo</button>
+            <button onClick={()=> fondo(2)} type="button" class="btn btn-success mx-2 mt-3">Fondo verde</button>
+            </div>
+        </div>
+
     );
 };
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,44 +1,13 @@
-import React, { useEffect, useState } from "react";
-import {Numbers} from "./Numbers.jsx";
+import React from "react";
+import { Numbers } from "./Numbers";
 
-
-const Home = () => {
-
-	const[timer, setTimer] = useState(0)
-	const [active, setActive] = useState(false)
-
-	useEffect(()=>{
-		if(active){
-			setTimeout(()=>{
-				setTimer (value => value + 1)
-			}, 1000)
-		}
-			
-		},[timer,active])
-
-	const startStop = () => setActive(value => !value)
-
-	const resetTimer = () => setTimer (value => value=0)
-
-	return (
-		<div className="text-center bg-primary p-5">
-			<h1 className="p-2 text-warning"> Seconds Counter </h1>
-			<div className="counter">
-				<Numbers number={<i className="far fa-clock"></i>}/>
-				<Numbers number={Math.floor(timer/100000)%10}/>
-				<Numbers number={Math.floor(timer/10000)%10}/>
-				<Numbers number={Math.floor(timer/1000)%10}/>
-				<Numbers number={Math.floor(timer/100)%10}/>
-				<Numbers number={Math.floor(timer/10)%10}/>
-				<Numbers number={Math.floor(timer)%10}/>
+	export const Home = ()=>{
+		return(
+			<div className="text-center mx-auto">
+				<Numbers/>
 			</div>
-			<div className="container text-center">
-				<button disabled={active} onClick={startStop} type="button" className="btn btn-success mx-2 mt-4">Start</button>
-				<button disabled={!active} onClick={startStop} className="btn btn-danger mx-2 mt-4">Stop</button>
-				<button onClick={resetTimer} className="btn btn-warning mx-2 mt-4">Reset</button>
-			</div>
-		</div>
-	);
-};
+		)
+	}
+	
 
 export default Home;
